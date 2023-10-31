57c2039dade7f30fd80b3d25d311d93f3d9aeb8e
Kristina <kristina.maximova@edu.edu.mt>
2023-10-31 11:04:13 +0100

    New style.css file + styled image and button. Calling generateDog on page load.
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Úkol 1: Použij API, GET na https://dog.ceo/api/breeds/image/random na získání náhodného obrázku psa.
 
+window.onload = function () {
+  generateDog();
+};
+
 async function generateDog() {
   try {
     var response = await fetch("https://dog.ceo/api/breeds/image/random");
@@ -7,6 +11,7 @@ async function generateDog() {
 
     // TODO: ask what data.status exactly does?
     if (data.status) {
+      console.log(data);
       document.getElementById("dog-image").src = data.message;
     } else {
       throw "Something went wrong!";
