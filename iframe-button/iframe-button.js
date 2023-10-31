7f0451bdd2f1a30f899551246aab422cd21b1de0
Kristina <kristina.maximova@edu.edu.mt>
2023-10-31 13:42:49 +0100

    Create 2 frames for button and image and save data to local storage so they can comunicate.
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- iframe-button/iframe-button.js
+++ iframe-button/iframe-button.js
@@ -1,1 +1,13 @@
-// Úkol 2: Logika iframu pro zobrazení tlačítka a API callu
+var dogData;
+
+async function generateDogButton() {
+  try {
+    var response = await fetch("https://dog.ceo/api/breeds/image/random");
+    dogData = await response.json();
+
+    // Save fetched data to local storage as JSON
+    localStorage.setItem("dogData", JSON.stringify(dogData));
+  } catch (error) {
+    console.error(error);
+  }
+}
--- iframe-img/iframe-img.js
+++ iframe-img/iframe-img.js
@@ -1,1 +1,20 @@
-// Úkol 2: Logika iframu pro zobrazení obrázku
+// Listen for localStorage change to generate new dog img
+window.addEventListener("storage", () => {
+  generateDogImage();
+});
+
+// Init first image load
+generateDogImage();
+
+async function generateDogImage() {
+  var parsedData = JSON.parse(window.localStorage.dogData);
+  try {
+    if (parsedData.status == "success") {
+      document.getElementById("dog-image").src = parsedData.message;
+    } else {
+      throw "Something went wrong!";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
