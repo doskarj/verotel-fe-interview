fcd40839a0b4bbbcd33a36257935d157a86b6840
Kristina <kristina.maximova@edu.edu.mt>
2023-10-31 10:33:34 +0100

    Add onclick listener to button + geterateDog function to fetch data for dog image
    Signed-off-by: Kristina <kristina.maximova@edu.edu.mt>

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Úkol 1: Použij API, GET na https://dog.ceo/api/breeds/image/random na získání náhodného obrázku psa.
+
+async function generateDog() {
+  try {
+    var response = await fetch("https://dog.ceo/api/breeds/image/random");
+    var data = await response.json();
+
+    // TODO: ask what data.status exactly does?
+    if (data.status) {
+      return data.message;
+    } else {
+      throw "Something went wrong!";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
